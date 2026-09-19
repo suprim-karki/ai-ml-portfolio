5b94194b20059d31136fd5f38890082f7917c0ff
Suprim Karki <shupreem1911@gmail.com>
2026-09-19 14:14:22 +0545

    Streamlit Inputs

--- machine-learning/projects/insurance-analysis/app.py
+++ machine-learning/projects/insurance-analysis/app.py
@@ -8,4 +8,9 @@ excepted_columns = joblib.load("columns_insurance.pkl")
 st.title("Insurance Premium Predictor 💵")
 st.markdown("Provide the following details.")
 
-
+age = st.slider("Age", 0, 110, 40)
+sex = st.selectbox("Sex", ["M","F"])
+children = st.number_input("Number of Childrens", 0,8,0)
+smoker = st.selectbox("Smoker",["Y","N"])
+region = st.selectbox("Region",["southeast","southwest","northwest","northeast"])
+bmi_category = st.selectbox("BMI Category", ["Obese","Overweight","Normal","Underweight"])
